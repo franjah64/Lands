cec0c35439edfd053600c1bb67a6234c4aae8ca7
Francisco Aguirre <franciscojaguirreh@gmail.com>
2018-02-11 20:07:14 -0500

    Fin de la primer clase

---
+++ Lands/Lands/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace Lands.Infrastructure
+{
+    using ViewModels;
+
+    class InstanceLocator
+    {
+        #region properties
+        public MainViewModel Main { get; set; }
+        #endregion
+
+        #region Constructores
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+        #endregion
+    }
+}
